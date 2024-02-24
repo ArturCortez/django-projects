0cf8c6a70303365c007cf1ec4db81bda07e366a4
ACortex <arturcortez.ms@gmail.com>
2024-02-24 13:50:30 +0000

    added chapter6-14 newspaper app

---
+++ django-for-beginners/chapter6-14/articles/admin.py
@@ -0,0 +1,18 @@
+# articles/admin.py
+
+from django.contrib import admin
+from .models import Article, Comment 
+
+class CommentInLine(admin.TabularInline): 
+    model = Comment
+    extra = 0 # new
+
+class ArticleAdmin(admin.ModelAdmin):
+    
+    inlines = [ 
+        CommentInLine,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment) 
